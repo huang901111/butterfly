c2ffc81a95116d6baaa74f83158a9165f8a52937
meetbill <meetbill@163.com>
2019-08-08 20:36:53 +0800

    [Update]:update examples

--- examples/client/py_tpl.py
+++ examples/client/py_tpl.py
@@ -9,16 +9,22 @@
 
 """
 import urllib2
+x_addr="127.0.0.1:8585"
 
-request = urllib2.Request("http://127.0.0.1:8585/ping")
-#request.add_header('content-TYPE', 'application/x-www-form-urlencoded')
-response = urllib2.urlopen(request)
-# 返回码
-print "ret_code:",response.getcode()
-print "url:",response.geturl()
-# 返回内容
-print "content:",response.read()
-# 请求id，此 id 会打印到 butterfly 日志中
-print "x-reqid:",response.info().getheader('x-reqid')
-# 请求耗时
-print "x-cost:",response.info().getheader('x-cost')
+def check(path):
+    request = urllib2.Request("http://{x_addr}{path}".format(x_addr = x_addr,path=path))
+    #request.add_header('content-TYPE', 'application/x-www-form-urlencoded')
+    response = urllib2.urlopen(request)
+    # 返回码
+    print "ret_code:",response.getcode()
+    print "url:",response.geturl()
+    # 返回内容
+    print "content:",response.read()
+    # 请求id，此 id 会打印到 butterfly 日志中
+    print "x-reqid:",response.info().getheader('x-reqid')
+    # 请求耗时
+    print "x-cost:",response.info().getheader('x-cost')
+
+if __name__ == "__main__":
+    for path in ["/x/ping","/apidemo/ping"]:
+        check(path)
